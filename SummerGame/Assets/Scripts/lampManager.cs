1e6999f89e045094199e1790dd186b1594d20509
asendlakowski <alana.sendlakowski@tufts.edu>
2023-08-25 00:18:42 -0700

    Starting to figure out logic of puzzle 1

--- SummerGame/Assets/Scripts/lampManager.cs
+++ SummerGame/Assets/Scripts/lampManager.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class lampManager : MonoBehaviour
 {
+    //private not show up in unity can't be accessed anywhere else
     private int currMask;
     private int solutionMask;
+    //[SerializeField] means shows up in unity and can't be accessed anywhere else
     [SerializeField] private bool[] correctLamps;
     public GameObject solutionObject;
     [SerializeField] private bool verticalDoorSolve;
     [SerializeField] private bool revealLightSolve;
     [SerializeField] private float doorHeight;
     private bool hasBeenSolved;
+    [SerializeField] private bool isFivebyFive;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,7 @@ public class lampManager : MonoBehaviour
         hasBeenSolved = false;
         currMask = 0;
         solutionMask = 0;
+        //builds the solution mask to be used to check if correct
         for (int i = 0; i < correctLamps.Length; i++) {
             if (correctLamps[i]) {
                 solutionMask += 1 << i;
@@ -33,6 +37,28 @@ public class lampManager : MonoBehaviour
         } else {
             currMask -= 1 << lampID;
         }
+        if (isFivebyFive && activating) { //only needs to check if new lamp has been turned on
+            //check row repeats
+            int row = lampID / 5;
+            for (int i = 0; i < 5; i++) {
+                if (((currMask & (1 << (row * 5 + i))) == 1) && ((row * 5 + i) != lampID)) { //should check if index i in row is on and also that on lamp was not just turned on
+                    currMask -= 1 << (row * 5 + i);
+                }
+            }
+
+            //check col repeats
+            int col = lampID % 5;
+            for (int j = 0; j < 5; j++) {
+                if (((currMask & (1 << (5 * j + col))) == 1) && ((5 * j + col) != lampID)) { //should check if index i in row is on and also that on lamp was not just turned on
+                    currMask -= 1 << (5 * j + col);
+                }
+            }
+
+            //check group repeats
+            //going to need to hard code these in somehow
+            
+        }
+
         if ((currMask == solutionMask) && !hasBeenSolved) {
             hasBeenSolved = true;
             Debug.Log("Correct Combination");
